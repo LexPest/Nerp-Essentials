91e35131720a7858c0755b51cd0283aaa3c0bf2c
Alexey Mihailov <lexpest@intworks.xyz>
2020-05-17 19:31:34 +0500

    Removed unnessesary compilation units

--- source/ness/interface/ILinkedObserver.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019-2020 Alexey "LexPest" Mihailov. All rights reserved.
-//
-// Use of this source code is governed by a GNU LGPLv3 license
-// that can be found in the LICENSE file.
-
-#include "ILinkedObserver.h"
-
-using namespace nerp;
